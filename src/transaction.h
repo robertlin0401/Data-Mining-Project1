b0953f3c93827d2a38dd10da973f0b0af6baec87
robertlin0401 <robertlin0401@gmail.com>
2021-10-30 19:37:03 +0800

    Others: change variable name
    Declare in struct transaction.
    item_list_tail -> item_list_ptr

--- src/transaction.h
+++ src/transaction.h
@@ -10,7 +10,7 @@ typedef struct item {
 
 typedef struct transaction {
     Item *item_list_head;
-    Item *item_list_tail;
+    Item *item_list_ptr;
     struct transaction *next;
 } Transaction;
 
@@ -33,7 +33,7 @@ void new_transaction()
 {
     Transaction *new_transaction = (Transaction *)malloc(sizeof(Transaction));
     new_transaction->item_list_head = NULL;
-    new_transaction->item_list_tail = NULL;
+    new_transaction->item_list_ptr = NULL;
     new_transaction->next = NULL;
     if (!transaction_ptr) {
         transaction_head = transaction_ptr = new_transaction;
@@ -50,10 +50,10 @@ void insert_item(int itemID)
 {
     Item *item_ptr = new_item(itemID);
     if (!transaction_ptr->item_list_head) {
-        transaction_ptr->item_list_head = transaction_ptr->item_list_tail = item_ptr;
+        transaction_ptr->item_list_head = transaction_ptr->item_list_ptr = item_ptr;
     } else {
-        transaction_ptr->item_list_tail->next = item_ptr;
-        transaction_ptr->item_list_tail = item_ptr;
+        transaction_ptr->item_list_ptr->next = item_ptr;
+        transaction_ptr->item_list_ptr = item_ptr;
     }
 }
 
@@ -64,11 +64,11 @@ void print_all_transactions()
 {
     transaction_ptr = transaction_head;
     while (transaction_ptr) {
-        transaction_ptr->item_list_tail = transaction_ptr->item_list_head;
-        while (transaction_ptr->item_list_tail) {
-            printf("%d ", transaction_ptr->item_list_tail->itemID);
-            if (transaction_ptr->item_list_tail->next)
-                transaction_ptr->item_list_tail = transaction_ptr->item_list_tail->next;
+        transaction_ptr->item_list_ptr = transaction_ptr->item_list_head;
+        while (transaction_ptr->item_list_ptr) {
+            printf("%d ", transaction_ptr->item_list_ptr->itemID);
+            if (transaction_ptr->item_list_ptr->next)
+                transaction_ptr->item_list_ptr = transaction_ptr->item_list_ptr->next;
             else
                 break;
         }

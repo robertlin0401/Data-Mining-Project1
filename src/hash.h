08f844091bfe05e5d5b9e17f15fac010fe595f8b
robertlin0401 <robertlin0401@gmail.com>
2021-10-31 15:21:26 +0800

    Revise: hash and itemset data structure
    Declare these two data structures and their operations.

--- src/hash.h
+++ src/hash.h
@@ -1,6 +1,24 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <stdbool.h>
+#include "itemset.h"
 
+typedef struct hash {
+    void *ptr[10]; // Hash * or Itemset *
+    int length[10];
+    bool is_hash[10];
+} Hash;
+
+/* Allocate a new hash table and initialize its content. */
+Hash *new_hash();
+
+/* Split itemset list that reaches the maximum size, 
+ * and distribute each itemset to the hash table.
+ */
+void split_itemset_to_hash(Itemset *target, Hash *hash_ptr, int itemID_digit, int level);
+
+/* Insert a new itemset into the target candidate hash table. */
+void insert_itemset_into_hash(Hash *target, Itemset *itemset_ptr, int itemID_digit, int level);
 
 #endif
--- src/itemset.h
+++ src/itemset.h
@@ -1,6 +1,21 @@
 #ifndef ITEMSET_H
 #define ITEMSET_H
 
+#include "transaction.h"
 
+typedef struct itemset {
+    Item *item_list_head;
+    int count;
+    int length;
+    struct itemset *next;
+} Itemset;
+
+/* Allocate a new itemset and initialize its content. */
+Itemset *new_itemset();
+
+/* Allocate a new item, insert it into the target itemset at
+ * the position where it should be.
+ */
+void insert_item_into_itemset(Itemset *target, int itemID);
 
 #endif

31ab8ca3182bb19fce07fa0f2ed9a0832ae5540d
robertlin0401 <robertlin0401@gmail.com>
2021-10-31 14:09:14 +0800

    Revise: modify the data structure for future usage
    Add `int length` into `struct transaction`.

--- src/transaction.h
+++ src/transaction.h
@@ -11,6 +11,7 @@ typedef struct item {
 typedef struct transaction {
     Item *item_list_head;
     Item *item_list_ptr;
+    int length;
     struct transaction *next;
 } Transaction;
 
@@ -34,6 +35,7 @@ void new_transaction()
     Transaction *new_transaction = (Transaction *)malloc(sizeof(Transaction));
     new_transaction->item_list_head = NULL;
     new_transaction->item_list_ptr = NULL;
+    new_transaction->length = 0;
     new_transaction->next = NULL;
     if (!transaction_ptr) {
         transaction_head = transaction_ptr = new_transaction;
@@ -55,6 +57,7 @@ void insert_item(int itemID)
         transaction_ptr->item_list_ptr->next = item_ptr;
         transaction_ptr->item_list_ptr = item_ptr;
     }
+    transaction_ptr->length++;
 }
 
 /* Print out item list of each transaction, one 
@@ -64,6 +67,7 @@ void print_all_transactions()
 {
     transaction_ptr = transaction_head;
     while (transaction_ptr) {
+        printf("%2d | ", transaction_ptr->length);
         transaction_ptr->item_list_ptr = transaction_ptr->item_list_head;
         while (transaction_ptr->item_list_ptr) {
             printf("%d ", transaction_ptr->item_list_ptr->itemID);
